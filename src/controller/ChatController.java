1811ede34299af6f4850c8f17cce74576df7958a
Ian Erickson <19ierickson@gmail.com>
2018-10-12 13:00:30 -0600

    imported stuff

--- src/controller/ChatController.java
+++ src/controller/ChatController.java
@@ -1,9 +1,18 @@
 package controller;
 
+import model.ChatModel;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+
 public class ChatController
 {
 	public void start()
 	{
 		
 	}
+	
+	
+	
+	
 }
