c84f130fb5929b44bf3666d31af0ec7731421832
Ian Erickson <19ierickson@gmail.com>
2018-10-12 13:53:26 -0600

    moved things around

--- src/controller/ChatController.java
+++ src/controller/ChatController.java
@@ -9,10 +9,20 @@ public class ChatController
 {
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(null, "Yo, I'm jeff. I'm gonna try to be smart but be prepared for me to fail miserably. If you would like to stop at any time type 'quit''");
+		String responce = JOptionPane.showInputDialog(null, "What would you like to say to me?");
+		while(!response.equals(!quit))
+		{
+			
+		}
+		Talk();
 	}
 	
-	
+	public void Talk()
+	{
+		
+				
+	}
 	
 	
 }
