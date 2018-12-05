47cd8aeb6518b90e4eba3a33a67f1e3fc6f9831a
Ian Erickson <19ierickson@gmail.com>
2018-12-05 12:04:00 -0700

    added setupScrollPane and setupPanel

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -10,8 +10,9 @@ import java.awt.Font;
 public class ChatPanel extends JPanel
 {
 	private ChatController appController;
+	private SpringLayout appLayout;
 	private JButton chatButton;
-	private JButton checkedButton;
+	private JButton checkerButton;
 	private JButton loadButton;
 	private JButton saveButton;
 	private JTextField chatField;
@@ -22,15 +23,48 @@ public class ChatPanel extends JPanel
 	{
 		super();
 		
+		this.appController = appController;
+		appLayout = new SpringLayout();
 		
+		saveButton = new JButton("Save");
+		loadButton = new JButton("Load");
+		chatButton =  new JButton("Chat");
+		checkerButton = new JButton("Check Text");
+		
+		chatField = new JTextField("Talk to the bot here", 50);
+		chatArea = new JTextArea("Chat area", 20, 50);
+		chatPane = new JScrollPane();
+		
+		setupScrollPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
 	
+	//------------------------------------------------------------------------------\\
+
+	private void setupScrollPane()
+	{
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		
+		chatPane.setViewportView(chatArea);
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	}
+	
 	private void setupPanel()
 	{
-		
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800, 600));
+		this.setBackground(Color.LIGHT_GRAY);
+		this.add(chatPane);
+		this.add(chatButton);
+		this.add(saveButton);
+		this.add(loadButton);
+		this.add(checkerButton);
+		this.add(chatField);
 	}
 	
 	private void setupLayout()
