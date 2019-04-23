a707e64ee5be4805a11662be65eda0cfbb390912
Ian Erickson <19ierickson@gmail.com>
2019-04-23 13:53:46 -0600

    added ChatTwitter

---
+++ src/chat/model/ChatTwitter.java
@@ -0,0 +1,16 @@
+package chat.model;
+
+import chat.controller.ChatController;
+import twitter4j.TwitterFactory;
+
+public class ChatTwitter
+{
+	private ChatController app;
+	private Twitter chatTwitter;
+	
+	public ChatTwitter(ChatController app)
+	{
+		this.app = app;
+		this.chatTwitter = TwitterFactory.getSingleton();
+	}
+}
